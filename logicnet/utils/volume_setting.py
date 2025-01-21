a90324fca53c5eb43602be64d9e4166417afb224
mrbreo <breo.space@gmail.com>
2025-01-21 20:34:26 +0000

    feat: 🔄 upgrade bt version for dtao

--- logicnet/utils/volume_setting.py
+++ logicnet/utils/volume_setting.py
@@ -15,7 +15,7 @@ def get_rate_limit_per_validator(
     Calculate the rate limit for each validator based on the epoch volume and the stake of the validators.
     The rate limit is the number of requests that a validator can process in a single epoch.
     """
-    all_stakes = [stake for stake in metagraph.total_stake.tolist()]
+    all_stakes = [stake for stake in metagraph.total_stake]
     all_uids = [uid for uid in range(len(all_stakes))]
     valid_stakes = [stake for stake in all_stakes if stake >= min_stake]
     valid_uids = [uid for uid, stake in zip(all_uids, all_stakes) if stake >= min_stake]
@@ -26,14 +26,14 @@ def get_rate_limit_per_validator(
                 f"No validators with stake greater than {min_stake} found. "
                 "Assigning equal volume to all validators."
                 f"Total volume: {epoch_volume}"
-                f"Metagraph stake: {metagraph.total_stake.tolist()}"
+                f"Metagraph stake: {metagraph.total_stake}"
             )
         )
         valid_uids = all_uids
         valid_stakes = [0] * len(all_stakes)
         min_stake = 0
 
-    valid_stakes = torch.tensor(valid_stakes) + 1e-4
+    valid_stakes = torch.tensor(valid_stakes, dtype=torch.float32) + 1e-4
     normalized_valid_stakes = valid_stakes / valid_stakes.sum()
     volume_per_validator = epoch_volume * normalized_valid_stakes
     volume_per_validator = torch.floor(volume_per_validator)
